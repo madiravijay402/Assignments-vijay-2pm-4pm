eebed3399daa136ffbfdb38dc5b958cb9edaa977
madiravijay402 <vijaymadira681@gmail.com>
2025-12-05 22:33:22 +0530

    button

---
+++ day16/miniproject.js
@@ -0,0 +1,12 @@
+
+console.log("welcome everyone");
+console.log("today date is:"+ new Date());
+
+console.log("Name: Vijay");
+console.log("Age: 22");
+console.log("Course: Java Full Stack");
+console.log("Country: India");
+console.log("Goal: Get a good IT job");
+
+
+
