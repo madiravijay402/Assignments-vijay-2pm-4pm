1308a577205a2c8249e9a24f01e8879a67abaf1a
madiravijay402 <vijaymadira681@gmail.com>
2025-12-08 21:57:06 +0530

    declare

---
+++ day17 assignment/declare.js
@@ -0,0 +1,40 @@
+var name="vijay";
+console.log(name);
+let a="dhanush";
+a="barghav";
+console.log(a);
+
+const b="dhurandhur";
+console.log(b);
+
+{
+    var c="adivi";
+
+}
+console.log(c);
+ var d="fizz";
+ d="samz"; 
+ console.log(d);
+ 
+ var e="res";
+ var e="thanos";
+ console.log(e);
+
+//  let f="fauzi";
+//  let f="salaar"; impossible
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
