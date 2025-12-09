47b3bca2f42c3b12922efc3a0037184a895fd13e
madiravijay402 <vijaymadira681@gmail.com>
2025-12-09 14:49:01 +0530

    day

--- day17 assignment/practical tasks/grading.js
+++ day17 assignment/practical tasks/grading.js
@@ -24,3 +24,20 @@ else{
     console.log("suspended");
     
 }
+
+// switch 
+//  good for fixed option
+let day=1;
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    default:
+    console.log("invalid day");   
+}
