12e0a997a3400f83914031833639a270492e8ca0
madiravijay402 <vijaymadira681@gmail.com>
2025-12-14 22:12:05 +0530

    project

---
+++ day19 assignment/progressive project/functions.js
@@ -0,0 +1,21 @@
+function js(a,b,c){
+    if(c==1){
+        console.log(a+b);
+    }
+    else if(c==2){
+        console.log(a*b);
+    }
+    else if(c==3){
+        console.log(a-b);
+    }
+    else if(c==4){
+        console.log(a/b);
+    }
+    else if(c==5){
+        console.log(a%b);
+    }
+    else{
+        console.log("error");
+    }
+}
+js(2,4,5);
