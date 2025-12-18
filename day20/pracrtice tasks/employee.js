7bf0bdd3dc714cb5b6bd584a3fadca15f65283e3
madiravijay402 <vijaymadira681@gmail.com>
2025-12-18 10:08:27 +0530

    jsk

--- day20/pracrtice tasks/employee.js
+++ day20/pracrtice tasks/employee.js
@@ -7,5 +7,11 @@ class employee{
         console.log(`the employee details: name: ${this.name} salary: ${this.salary}`);
     }
 }
+class Manager extends employee{
+    constructor(department){
+        this.department=department;
+    }
+    
+}
 let c=new employee("vijay","18lpa")
 c.opd();
