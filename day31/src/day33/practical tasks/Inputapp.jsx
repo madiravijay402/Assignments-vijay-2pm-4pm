2caab0a0024e72bf1a50c64b5e13f6a0135d99e8
madiravijay402 <vijaymadira681@gmail.com>
2026-01-17 13:25:44 +0530

    day33

--- day31/src/day33/practical tasks/Inputapp.jsx
+++ day31/src/day33/practical tasks/Inputapp.jsx
@@ -2,16 +2,20 @@ import React,{useState} from 'react'
 
 function Inputapp() {
     const[input,setinput]=useState("")
+    const handlechange=(e)=>{
+        setinput({...input,[e.target.name]:e.target.value});
+    };
   return (
     <>
     <div>Inputapp</div>
     {/* <input type="text" onChange={(e)=>{setinput(e.input.value)}}/> */}
-    <p>preview : {input}</p>
-    <form action="" onChange={(e)=>{setinput(e.input.value)}}>
+    <p>preview : {input.name}</p>
+    <p>preview :{input.email}</p>
+    <form action="" >
         <label htmlFor="">name</label>
-        <input type="text" name='name'/>
-        <label htmlFor="">email</label>
-        <input type="email" name='email'/>
+        <input type="text" name="name"onChange={handlechange}/>
+        <label>email</label>
+        <input type="email" name="email" onChange={handlechange}/>
         <button type='submit'>login</button>
     </form>
     </>
