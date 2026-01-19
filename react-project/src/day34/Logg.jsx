4832987219d9a03c1835fb3ea9187597a3e2d2d3
madiravijay402 <vijaymadira681@gmail.com>
2026-01-19 20:18:57 +0530

    login logic

--- react-project/src/day34/Logg.jsx
+++ react-project/src/day34/Logg.jsx
@@ -2,10 +2,25 @@ import React, { useState } from 'react'
 
 function Logg() {
     const[log,setlog]=useState(false);
+    function success(e){
+      e.preventDefault();
+      if(e.target.email.value===" "){
+        alert("email is required")
+      }
+      if(e.target.password.value===" "){
+        alert("password is required")
+      }
+      if(e.target.email.value==="vijaymadira681@gmail.com" && e.target.password.value==="vijay123"){
+         alert("login successful")
+      }
+      else{
+        alert("login failed")
+      }
+    }
   return (
     <>
-    <div className="container" style={{width:200, height:200,backgroundColor:'black',display:'flex',flexDirection:'column',justifyContent:'center', gap:10,alignItems:'center'}}>
-    <form action="">
+    <div className="container" style={{width:200, height:200,backgroundColor:'black',display:'flex',flexDirection:'column',justifyContent:'center', gap:10,alignItems:'center',border:2,borderColor:'black',borderStyle:'solid',borderRadius:10,padding:20,marginTop:50}}>
+    <form onSubmit={success}>
     <label className='text-light'>email</label>
     <input type="email" name='email' /><br />
     <label className='text-light'>password</label>
