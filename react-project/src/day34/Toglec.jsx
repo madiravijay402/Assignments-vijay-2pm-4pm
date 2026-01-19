c5bff4cca938852d51f87ffa404ebd2a8b562aa3
madiravijay402 <vijaymadira681@gmail.com>
2026-01-19 23:04:21 +0530

    update style

--- react-project/src/day34/Toglec.jsx
+++ react-project/src/day34/Toglec.jsx
@@ -1,12 +1,22 @@
 import React, { useState, useTransition } from 'react'
-import './toggle.css'
+
 function Toglec() {
   const [test,settest]=useState(false)
+  const boxStyle = {
+    maxHeight: open ? "200px" : "0px",
+    opacity: open ? 1 : 0,
+    overflow: "hidden",
+    transition: "all 0.6s ease",
+    background: "linear-gradient(to right,#1d1d1d,#454545)",
+    color: "white",
+    marginTop: "20px",
+    padding: open ? "20px" : "0 20px"
+  };
 
   return (
     <>
       <button onClick={()=>{settest(!test)}}>{test ? "hide":"expand"}</button>
-      {test && <div className={`container ${test ? 'show':''}`}>
+      {test && <div style={boxStyle}>
         <h1 className='text-light'>used && for toggle </h1>
       </div>
       }
