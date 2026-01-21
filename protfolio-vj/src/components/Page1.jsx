4efa9c079dd71dbd014539b007f34feb6d5e8c09
madiravijay402 <vijaymadira681@gmail.com>
2026-01-21 20:47:32 +0530

    update

--- protfolio-vj/src/components/Page1.jsx
+++ protfolio-vj/src/components/Page1.jsx
@@ -4,9 +4,13 @@ import vjlogo from './vjlogo.png.png'
 function Page1() {
   return (
     <>
-      <nav className="navbar   bg-dark text-white">
+      <nav className="navbar bg-dark text-white">
         <div className="container-fluid">
-          <a className="navbar-brand text-white" href="#" style={{fontFamily:'-apple-system'}}>
+          <a
+            className="navbar-brand text-white"
+            href="#"
+            style={{ fontFamily: '-apple-system' }}
+          >
             <img
               src={vjlogo}
               alt="Logo"
@@ -14,21 +18,36 @@ function Page1() {
               height="24"
               className="d-inline-block align-text-top"
             />
-           Vijay Madira
+            Vijay Madira
           </a>
-           <div className="navbar-nav d-flex flex-row gap-4 text-captialize">
-            <a href="#" className='nav-link text-white'>home</a>
-            <a href="#" className='nav-link text-white'>skills</a>
-            <a href="#"className='nav-link text-white'>projects</a>
-           </div>
+
+          <div className="navbar-nav d-flex flex-row gap-4 text-capitalize">
+            <a href="#" className="nav-link text-white">home</a>
+            <a href="#" className="nav-link text-white">skills</a>
+            <a href="#" className="nav-link text-white">projects</a>
+          </div>
         </div>
       </nav>
+
       <div>
-      <div>
-        <h1 className='text-center mt-5 text-white' style={{fontFamily:'cursive'}}>Hello welcome to my portfolio !</h1>
-      </div>
-      <button>Resume</button>
-      <button>Github</button>
+        <h1
+          className="text-center mt-5 text-white"
+          style={{ fontFamily: 'cursive' }}
+        >
+          Hello welcome to my portfolio !
+        </h1>
+
+        <div className="text-center mt-3">
+          <button className="resume">
+            <a href="" className="nav-link">Resume</a>
+          </button>
+          <button className="github">
+            <a href="" className="nav-link">Github</a>
+          </button>
+          <button className="email">
+            <a href="" className="nav-link">Email</a>
+          </button>
+        </div>
       </div>
     </>
   )
