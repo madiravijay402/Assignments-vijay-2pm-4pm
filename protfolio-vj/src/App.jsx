5b81c84ed741312c60d2e7d2a826f3e6e98b5fdb
madiravijay402 <vijaymadira681@gmail.com>
2026-01-22 21:29:44 +0530

    update

--- protfolio-vj/src/App.jsx
+++ protfolio-vj/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Page1 from './components/Page1';
-import Page2 from './components/Page2';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Page1" element={<Page1 />} />
+        <Route path="/" element={<Page1 />} />
       </Routes>
     </BrowserRouter>
   );
