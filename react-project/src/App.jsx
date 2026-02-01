be2359e474d2593679023d3ee80cdf8553251eec
madiravijay402 <vijaymadira681@gmail.com>
2026-02-01 22:36:43 +0530

    update

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Input from './day36/Input';
-import Multiple from './day36/Multiple';
-import Reload from './day36/Reload';
-import Checkbox from './day36/Checkbox';
-import Check from './day36/Check';
-import Reset from './day36/Reset';
-import Login from './day36/progressive project/Login';
-import Sign from './day36/progressive project/Sign';
-import Profile from './day36/progressive project/Profile';
+import Input from './day35/Input';
+import Multiple from './day35/Multiple';
+import Reload from './day35/Reload';
+import Checkbox from './day35/Checkbox';
+import Check from './day35/Check';
+import Reset from './day35/Reset';
+import Login from './day35/progressive project/Login';
+import Sign from './day35/progressive project/Sign';
+import Profile from './day35/progressive project/Profile';
 function App() {
   return (
     <>
