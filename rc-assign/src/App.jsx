60ea81f948ad792afed6cf966c11c85ed2e35815
madiravijay402 <vijaymadira681@gmail.com>
2026-02-02 23:14:06 +0530

    update

--- rc-assign/src/App.jsx
+++ rc-assign/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import Home from './day37/Home'
+import About from './day37/About'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
