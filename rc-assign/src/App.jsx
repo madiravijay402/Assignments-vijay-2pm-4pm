e02bd98a6e6688d9758612a00caeb864106d151b
madiravijay402 <vijaymadira681@gmail.com>
2026-02-02 23:28:34 +0530

    update

--- rc-assign/src/App.jsx
+++ rc-assign/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 // import './App.css'
 import Home from './day37/Home'
 import About from './day37/About'
+import Contact from './day37/Contact'
 
 function App() {
 
@@ -12,10 +13,12 @@ function App() {
         <nav className="flex justify-center gap-10 p-4 bg-gray-900 shadow-md">
           <Link to="/" className="text-white text-lg font-medium hover:text-sky-400 transition">Home</Link> |{" "}
           <Link to="/about" className="text-white text-lg font-medium hover:text-sky-400 transition">About</Link>
+          <Link to="/about/contact" className="text-white text-lg font-medium hover:text-sky-400 transition">contact me</Link>
         </nav>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
+          <Route path='/about/contact' element={<Contact/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ rc-assign/src/day37/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className="max-w-xl mx-auto mt-10 p-6 bg-white rounded-xl shadow-lg">
+        <a href="mailto:vijaymadira681@gmail.com" className="text-2xl font-semibold mb-2">email</a>
+        <a href="#" className="text-gray-600">instagram</a>
+    </div>
+  )
+}
+
+export default Contact
