0fcb94cbeb526892431c6e93d0b6859208f0c932
madiravijay402 <vijaymadira681@gmail.com>
2026-02-05 15:50:36 +0530

    update

---
+++ react-project/src/app/store.js
@@ -0,0 +1,18 @@
+// const counterSlice=createSlice({
+//     name:'counter',
+//     initialState,
+//     reducers:{...}
+// })
+
+// no api calls
+//  no side effects
+//   returns updated state
+
+// actions - action is an event that tells redux something happaned 
+
+// dispatch - this used to send action to redux 
+
+//selectoroe is used to read state from the store 
+const count=usSele(state=>state.count.value)
+
+// initial sate is the default value of slice of the state
--- react-project/src/redex.jsx
+++ react-project/src/redex.jsx
@@ -2,7 +2,22 @@
   
   function redex() {
     return (
-      <div>redex</div>
+      <div>
+        redex is a state management libraray used with react to manage global application state 
+        (data shered cross components)
+
+        store - store is the centerlised price  where all application state is stored
+        only one store for react app 
+
+        slice - slice is pieace of redux state + logic for updating state 
+
+        combines state+reduce+action
+
+        reduces boiler plate 
+        feature - based structure
+
+        reducer - function updates the state based on an action
+      </div>
     )
   }
   
